f7f5b4e3de345719837975de1a97682021dc35bd
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2023-07-14 20:30:44 +0700

    pretty good

--- main.py
+++ main.py
@@ -23,16 +23,17 @@ def find(arr, n, m, x, y, b, dem, demN, sub_arr):
         if arr[i][b] == 0:
             if long(arr, x, y, i, b) == 0:
                 demT = dem + (abs(x-i) + 1)
+                # print(demT)
                 for l in range(min(x, i)+1, max(x, i)+1):
                     new_sub_arr.append([l,(b if x>i else y)])
                 new_sub_arr.append([i,b])
+                # print(new_sub_arr)
                 if y != m-2:
                     demN = find(arr, n, m, i, b, b+1, demT, demN,new_sub_arr)
                 else:
                     if demT < demN:
                         global main_arr_path
-                        main_arr_path = sub_arr.copy()
-                        main_arr_path.append([i, y+1])
+                        main_arr_path = new_sub_arr.copy()
                     demN = min(demN, demT)
 
     return demN
@@ -46,13 +47,13 @@ def find(arr, n, m, x, y, b, dem, demN, sub_arr):
 #      ]
 # n = 6
 # m = 6
-arr =  [[0, 1, 0, 1, 1, 1, 1, 1],
-        [1, 1, 0, 1, 0, 0, 0, 1],
-        [1, 1, 1, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 1, 1, 0, 0]]
-n = 5
-m = 8
+# arr =  [[0, 1, 0, 1, 1, 1, 1, 1],
+#         [1, 1, 0, 1, 0, 0, 0, 1],
+#         [1, 1, 1, 0, 0, 1, 0, 1],
+#         [1, 0, 0, 0, 0, 1, 0, 0],
+#         [0, 0, 0, 0, 1, 1, 0, 0]]
+# n = 5
+# m = 8
 # --------
 # [[0 1 0 1 1 1 1 1],
 #  [1 1 0 1 0 0 0 1],
@@ -60,11 +61,11 @@ m = 8
 #  [1 0 0 0 0 1 0 0],
 #  [0 0 0 0 1 1 0 0]] output 11 should 13
 # --------
-# n = random.randint(3,10)
-# m = random.randint(3,10)
-# arr = np.random.rand(n, m)
-# arr = np.where(arr > 0.5, 1, 0)
-# print(arr)
+n = random.randint(3,10)
+m = random.randint(3,10)
+arr = np.random.rand(n, m)
+arr = np.where(arr > 0.5, 1, 0)
+print(arr)
 
 demN = n*m
 for i in range(0, n):
